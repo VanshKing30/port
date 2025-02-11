d26db9c6d0d86d8aae4d0f3bae0dfc5989cb9646
Vansh <vanshwaldeo360@gmail.com>
2025-02-12 00:25:48 +0530

    Repo Started

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <>
+      <h1>Vansh Waldeo</h1>
+      <Button variant="link">CLick Me</Button>
+      
+    </>
+  );
+}
