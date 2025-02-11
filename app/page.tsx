7902ecbeac2e92e5e93fdd0249ddb547b4d09388
Vansh <vanshwaldeo360@gmail.com>
2025-02-12 00:50:36 +0530

    Added theme toggle and profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
-import Image from "next/image";
+"use client"
 
 import { Button } from "@/components/ui/button";
-
+import Image from "next/image";
+import pp from '../public/myImageLol.jpg'
+import ThemeToggle from '../components/ui/theme-toggle';
 
 export default function Home() {
   return (
-    <>
-      <h1>Vansh Waldeo</h1>
-      <Button variant="link">CLick Me</Button>
-      
-    </>
+    <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
+      <div className="mx-auto max-w-xl px-4 py-20">
+
+        <header className="flex items-ceter justify-between mb-12">
+          <div className="w-20 h-20 rounded-full overflow-hidden">
+            <Image src={pp} alt="Profile Picture" className="cursor-pointer transition-all duration-300 hover:scale-110"></Image>
+          </div>
+          <ThemeToggle/>
+        </header>
+
+      </div>
+    </div>
   );
 }
