46df60381c8a861228eb7924a6a6e9ff58a5e464
Vansh <vanshwaldeo360@gmail.com>
2025-02-12 19:12:05 +0530

    dummy content

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,17 @@ import ThemeToggle from '../components/ui/theme-toggle';
 import {motion} from "motion/react"
 import { FaGithub, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
+import { Card, CardContent } from "@/components/ui/card";
+
+
 
 
 export default function Home() {
   return (
+
     <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300">
       <div className="mx-auto max-w-xl px-4 py-20">
-
+      
         <motion.header 
           initial ={{opacity : 0 , x :-100 }}
           animate ={{opacity : 1 , x : 0}}
@@ -25,7 +29,8 @@ export default function Home() {
           </div>
           <ThemeToggle/>
         </motion.header>
-
+      
+      
         <main className="space-y-1">
 
           <section className="space-y-10">
@@ -62,7 +67,7 @@ export default function Home() {
             </p>
 
           </motion.div>
-
+         
           <motion.div
           initial={{opacity : 0 , x : 50}}
           animate={{opacity : 1 , x : 0}}
@@ -71,7 +76,7 @@ export default function Home() {
             <Button className="rounded-full bg-gradient-to-r from-rose-600 to-indigo-600 text-white transition-transform hover:scale-105">Resume</Button>
 
 
-            <Link href="#" className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors">
+            <Link href="https://github.com/VanshKing30/port" className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors">
             <FaGithub className="w-6 h-6"/>
             </Link>
 
@@ -114,12 +119,96 @@ export default function Home() {
 
           </motion.section>
 
+          <motion.section
+             initial={{opacity : 0 , y : 100}}
+             animate={{opacity : 1 , y : 0}}
+             transition={{duration : 0.8}}
+             className="space-y-8">
+
+              <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">Education</h2>
+
+              <div className="space-y-2">
+                <div className="flex items-center justify-between">
+                  <div className="space-y-1">
+                    <p className="text-sm text-gray-600 dark:text-gray-400">July , 2022-2024</p>
+                    <p className="font-medium">Software Education</p>
+                    <p className="text-blue-600 dark:text-gray-400">TCS</p>
+                  </div>
+                </div>
+                <p className="text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique enim odio, quas veniam incidunt laudantium, ea deleniti eligendi vero repellat sequi iusto nihil blanditiis, cum repellendus mollitia a dolores doloribus sunt error in sint ipsum? Inventore delectus nisi similique voluptatem quos beatae ratione tenetur iusto assumenda, amet dolorum earum placeat?</p>
+              </div>
+          </motion.section>
+
+
+          <motion.section
+          initial={{opacity : 0 , y : 100}}
+          animate={{opacity : 1 , y : 0}}
+          transition={{duration : 0.8}}
+          className="space-y-8">
+
+              <h2 className="text-3xl font-semibold bg-gradient-to-r from-rose-600 via-indigo-500 to-sky-500 bg-clip-text text-transparent inline-block">Projects</h2>
+
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+                <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                  <CardContent className="p-4">
+                    <Image src={pp} alt="project 1" className="rounded-lg mb-4"/>
+                      <div className="flex-items-center justify-between">
+                        <div>
+                          <h3 className="font-medium">Project 1</h3>
+                          <p className="text-sm text-gray-600 dark:text-gray-400">Next.js , MySQL , Drizzele</p>
+                        </div>
+                        <Button variant="ghost" size="icon">+</Button>
+                      </div>
+                    
+                  </CardContent>
+                </Card>
+
+                <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                  <CardContent className="p-4">
+                    <Image src={pp} alt="project 1" className="rounded-lg mb-4"/>
+                      <div className="flex-items-center justify-between">
+                        <div>
+                          <h3 className="font-medium">Project 1</h3>
+                          <p className="text-sm text-gray-600 dark:text-gray-400">Next.js , MySQL , Drizzele</p>
+                        </div>
+                        <Button variant="ghost" size="icon">+</Button>
+                      </div>
+                    
+                  </CardContent>
+                </Card>
+
+                <Card className="bg-gray-50 dark:bg-zinc-900 border-gray-200 dark:border-zinc-800 transition-transform duration-300 hover:scale-105 cursor-pointer">
+                  <CardContent className="p-4">
+                    <Image src={pp} alt="project 1" className="rounded-lg mb-4"/>
+                      <div className="flex-items-center justify-between">
+                        <div>
+                          <h3 className="font-medium">Project 1</h3>
+                          <p className="text-sm text-gray-600 dark:text-gray-400">Next.js , MySQL , Drizzele</p>
+                        </div>
+                        <Button variant="ghost" size="icon">+</Button>
+                      </div>
+                    
+                  </CardContent>
+                </Card>
+
+                
+              </div>
+                          
+            
+            
+            
+            
+            
+            <p className="text-gray-500">Made with ❤️ by Vansh</p>
+            </motion.section>   
+
 
           
           
         </main>
 
       </div>
+    
     </div>
   );
 }
